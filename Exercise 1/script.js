e50a3f945e55dd7b3800ed5543b18ef94317c0c6
igblui <60222867+igblui@users.noreply.github.com>
2020-08-30 21:49:15 +0800

    Add files via upload

---
+++ Exercise 1/script.js
@@ -0,0 +1,10 @@
+
+     
+function getInputValue() {
+    // Selecting the input element and getting its value
+    var inputVal = document.getElementById('myInput').value;
+    // Attaching the value to h2
+    document.getElementById('heading2').innerHTML = inputVal;     
+}
+         
+  
